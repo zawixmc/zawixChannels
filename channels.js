7ecd5549c2c48c62963e58282cb132fd4c2e854d
zawrotmc <ulmanekkacperek@gmail.com>
2025-09-06 19:56:54 +0200

    NO ADS CHANNELS

--- channels.js
+++ channels.js
@@ -1,68 +1,110 @@
 const channelsData = {
     1: [{
         "name": "Canal+ Sport 1",
-        "url": "https://thedaddy.top/embed/stream-48.php",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=14",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     2: [{
         "name": "Canal+ Sport 2",
-        "url": "https://thedaddy.top/embed/stream-73.php",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=15",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     3: [{
         "name": "Canal+ Sport 3",
-        "url": "https://thedaddy.top/embed/stream-259.php",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=16",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     4: [{
-        "name": "Canal+ Sport 5",
-        "url": "https://thedaddy.top/embed/stream-75.php",
+        "name": "Canal+ Sport 4",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=17",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     5: [{
-        "name": "Eleven Sports 1",
-        "url": "https://thedaddy.top/embed/stream-71.php",
+        "name": "Canal+ Sport 5",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=18",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     6: [{
-        "name": "Eleven Sports 2",
-        "url": "https://thedaddy.top/embed/stream-72.php",
+        "name": "Canal+ Extra 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=8",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     7: [{
-        "name": "Eleven Sports 3",
-        "url": "https://thedaddy.top/embed/stream-428.php",
+        "name": "Canal+ Extra 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=9",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     8: [{
-        "name": "Polsat Sport 1",
-        "url": "https://thedaddy.top/embed/stream-47.php",
+        "name": "Eleven Sports 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=19",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     9: [{
-        "name": "Polsat Sport 2",
-        "url": "https://thedaddy.top/embed/stream-50.php",
+        "name": "Eleven Sports 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=20",
         "country": "PL",
         "language": "Polski",
         "quality": "ULTRA HD"
     }],
     10: [{
+        "name": "Eleven Sports 3",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=21",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    11: [{
+        "name": "Eleven Sports 4",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=22",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    12: [{
+        "name": "Polsat Sport 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=25",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    13: [{
+        "name": "Polsat Sport 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=26",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    14: [{
+        "name": "Polsat Sport Premium 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=29",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    15: [{
+        "name": "Polsat Sport Premium 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=30",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    16: [{
         "name": "TVP Sport",
         "url": "https://thedaddy.top/embed/stream-128.php",
         "country": "PL",
