a3b733c621391b46bc6861b30622b2c361d22328
zawrotmc <ulmanekkacperek@gmail.com>
2025-09-11 21:35:12 +0200

    FIX

---
+++ channels-notworking.js
@@ -0,0 +1,114 @@
+const channelsData = {
+    1: [{
+        "name": "Canal+ Sport 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=14",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    2: [{
+        "name": "Canal+ Sport 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=15",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    3: [{
+        "name": "Canal+ Sport 3",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=16",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    4: [{
+        "name": "Canal+ Sport 4",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=17",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    5: [{
+        "name": "Canal+ Sport 5",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=18",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    6: [{
+        "name": "Canal+ Extra 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=8",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    7: [{
+        "name": "Canal+ Extra 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=9",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    8: [{
+        "name": "Eleven Sports 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=19",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    9: [{
+        "name": "Eleven Sports 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=20",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    10: [{
+        "name": "Eleven Sports 3",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=21",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    11: [{
+        "name": "Eleven Sports 4",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=22",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    12: [{
+        "name": "Polsat Sport 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=25",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    13: [{
+        "name": "Polsat Sport 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=26",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    14: [{
+        "name": "Polsat Sport Premium 1",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=29",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    15: [{
+        "name": "Polsat Sport Premium 2",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=30",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }],
+    16: [{
+        "name": "TVP Sport",
+        "url": "https://1.zhdcdn.zip/strumyk.php?a=32",
+        "country": "PL",
+        "language": "Polski",
+        "quality": "ULTRA HD"
+    }]
+};
