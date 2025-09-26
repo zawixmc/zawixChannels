a0bca4f71107a2c27034ae1565528ba59f47ad56
zawixmc <ulmanekkacperek@gmail.com>
2025-09-26 17:51:49 +0200

    PERFORMANCE

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static('.'));
