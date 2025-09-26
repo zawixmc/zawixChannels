ae3265bccb5125ca820b457c26bb345bcfeab7f5
zawixmc <ulmanekkacperek@gmail.com>
2025-09-26 17:56:26 +0200

    PERFORMANCE

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -14,6 +14,34 @@ app.get('/', (req, res) => {
 
 app.get('/channels', (req, res) => {
     res.sendFile(path.join(__dirname, 'channels', 'index.html'));
+});
+
+app.get('/channels/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'channels', 'index.html'));
+});
+
+app.get('/channels/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'channels', 'index.html'));
+});
+
+app.get('/channels/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'channels', 'script.js'));
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+});
+
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'styles.css'));
+});
+
+app.get('/channels.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'channels.js'));
+});
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'favicon.ico'));
 });
 
 app.post('/save-channels', (req, res) => {
